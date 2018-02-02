9ea6abf4d64a7c3b613e26f19636730edf63f97c
Katkofil <kat.kofil@yahoo.fr>
2018-02-02 12:17:39 +0100

    final cree_tableau

--- create_grid.c
+++ create_grid.c
@@ -266,11 +266,17 @@ int ft_boat_1_6(char **grid, int indice_col, int indice_line){
 	int conteur_col = indice_col;
 	int conteur_line = indice_line;
 
+	if (ft_test_boat(grid, conteur_col, conteur_line) != 0)
+		return 1;
 	if (ft_test_born(conteur_col, conteur_line - 1) == 0){
 		if (ft_test_boat(grid, conteur_col, conteur_line - 1) != 0)
 			return 1;
 	}
-	if (ft_test_born(conteur_col - 1, conteur_line) == 0){
+	if (ft_test_born(conteur_col, conteur_line + 1) == 0){
+		if (ft_test_boat(grid, conteur_col, conteur_line + 1) != 0)
+			return 1;
+	}
+if (ft_test_born(conteur_col - 1, conteur_line) == 0){
 		if (ft_test_boat(grid, conteur_col - 1, conteur_line) != 0)
 			return 1;
 	}
@@ -350,6 +356,265 @@ int ft_boat_1_6(char **grid, int indice_col, int indice_line){
 	return 1;
 }
 
+int ft_boat_2_2(char **grid, int indice_col, int indice_line){
+	int conteur_col = indice_col;
+	int conteur_line = indice_line;
+
+	if (ft_test_boat(grid,  conteur_col, conteur_line) != 0)
+		return 1;
+	if (ft_test_born(conteur_col - 1, conteur_line) == 0){
+		if (ft_test_boat(grid, conteur_col - 1, conteur_line) != 0)
+			return 1;
+	}
+	if (ft_test_born(conteur_col, conteur_line - 1) == 0){
+		if (ft_test_boat(grid, conteur_col, conteur_line - 1) != 0)
+			return 1;
+	}
+	if (ft_test_born(conteur_col + 1, conteur_line) == 0){
+		if (ft_test_boat(grid, conteur_col + 1, conteur_line) != 0)
+			return 1;
+	}
+	if (ft_test_born(conteur_col + 2, conteur_line) == 0){
+		if (ft_test_boat(grid, conteur_col + 2, conteur_line) != 0)
+			return 1;
+	}
+	if (ft_test_born(conteur_col + 1, conteur_line - 1) == 0){
+		if (ft_test_boat(grid, conteur_col + 1, conteur_line - 1) != 0)
+			return 1;
+	}
+	if (ft_test_born(conteur_col, conteur_line + 1) == 0){
+		if (ft_test_boat(grid, conteur_col, conteur_line + 1) != 0)
+			return 1;
+	}
+	if (ft_test_born(conteur_col - 1, conteur_line + 1) == 0){
+		if (ft_test_boat(grid, conteur_col - 1, conteur_line + 1) != 0)
+			return 1;
+	}
+	if (ft_test_born(conteur_col, conteur_line + 2) == 0){
+		if (ft_test_boat(grid, conteur_col, conteur_line + 2) != 0)
+			return 1;
+	}
+	if (ft_test_born(conteur_col + 1, conteur_line + 1) == 0){
+		if (ft_test_boat(grid, conteur_col + 1, conteur_line + 1) != 0)
+			return 1;
+	}
+	if (ft_test_born(conteur_col + 2, conteur_line + 1) == 0){
+		if (ft_test_boat(grid, conteur_col + 2, conteur_line + 1) != 0)
+			return 1;
+	}
+	if (ft_test_born(conteur_col + 1, conteur_line + 2) == 0){
+		if (ft_test_boat(grid, conteur_col + 1, conteur_line + 2) != 0)
+			return 1;
+	}
+	if (indice_col < 15 && indice_line < 15){
+		grid[indice_line][indice_col] = 'O';
+		grid[indice_line + 1][indice_col] = 'O';
+		grid[indice_line][indice_col + 1] = 'O';
+		grid[indice_line + 1][indice_col + 1] = 'O';
+		return 0;
+	}
+	return 1;
+}
+
+int ft_boat_2_4(char **grid, int indice_col, int indice_line){
+	int conteur_col = indice_col;
+	int conteur_line = indice_line;
+
+	if (ft_test_boat(grid,  conteur_col, conteur_line) != 0)
+		return 1;
+	if (ft_test_born(conteur_col - 1, conteur_line) == 0){
+		if (ft_test_boat(grid, conteur_col - 1, conteur_line) != 0)
+			return 1;
+	}
+	if (ft_test_born(conteur_col, conteur_line - 1) == 0){
+		if (ft_test_boat(grid, conteur_col, conteur_line - 1) != 0)
+			return 1;
+	}
+
+	if (ft_test_born(conteur_col + 1, conteur_line) == 0){
+		if (ft_test_boat(grid, conteur_col + 1, conteur_line) != 0)
+			return 1;
+	}
+	if (ft_test_born(conteur_col + 1, conteur_line - 1) == 0){
+		if (ft_test_boat(grid, conteur_col + 1, conteur_line - 1) != 0)
+			return 1;
+	}
+
+	if (ft_test_born(conteur_col + 2, conteur_line) == 0){
+		if (ft_test_boat(grid, conteur_col + 2, conteur_line) != 0)
+			return 1;
+	}
+	if (ft_test_born(conteur_col + 2, conteur_line - 1) == 0){
+		if (ft_test_boat(grid, conteur_col + 2, conteur_line - 1) != 0)
+			return 1;
+	}
+
+	if (ft_test_born(conteur_col + 3, conteur_line) == 0){
+		if (ft_test_boat(grid, conteur_col + 3, conteur_line) != 0)
+			return 1;
+	}
+	if (ft_test_born(conteur_col + 3, conteur_line - 1) == 0){
+		if (ft_test_boat(grid, conteur_col + 3, conteur_line - 1) != 0)
+			return 1;
+	}
+	if (ft_test_born(conteur_col + 4, conteur_line) == 0){
+		if (ft_test_boat(grid, conteur_col + 4, conteur_line) != 0)
+			return 1;
+	}
+
+	if (ft_test_born(conteur_col, conteur_line + 1) == 0){
+		if (ft_test_boat(grid, conteur_col, conteur_line + 1) != 0)
+			return 1;
+	}
+	if (ft_test_born(conteur_col - 1, conteur_line + 1) == 0){
+		if (ft_test_boat(grid, conteur_col - 1, conteur_line + 1) != 0)
+			return 1;
+	}
+	if (ft_test_born(conteur_col, conteur_line + 2) == 0){
+		if (ft_test_boat(grid, conteur_col, conteur_line + 2) != 0)
+			return 1;
+	}
+
+	if (ft_test_born(conteur_col + 1, conteur_line + 1) == 0){
+		if (ft_test_boat(grid, conteur_col + 1, conteur_line + 1) != 0)
+			return 1;
+	}
+	if (ft_test_born(conteur_col + 1, conteur_line + 2) == 0){
+		if (ft_test_boat(grid, conteur_col + 1, conteur_line + 2) != 0)
+			return 1;
+	}
+
+	if (ft_test_born(conteur_col + 2, conteur_line + 1) == 0){
+		if (ft_test_boat(grid, conteur_col + 2, conteur_line + 1) != 0)
+			return 1;
+	}
+	if (ft_test_born(conteur_col + 2, conteur_line + 2) == 0){
+		if (ft_test_boat(grid, conteur_col + 2, conteur_line + 2) != 0)
+			return 1;
+	}
+
+	if (ft_test_born(conteur_col + 3, conteur_line + 1) == 0){
+		if (ft_test_boat(grid, conteur_col + 3, conteur_line + 1) != 0)
+			return 1;
+	}
+	if (ft_test_born(conteur_col + 3, conteur_line + 2) == 0){
+		if (ft_test_boat(grid, conteur_col + 3, conteur_line + 2) != 0)
+			return 1;
+	}
+	if (ft_test_born(conteur_col + 3, conteur_line + 4) == 0){
+		if (ft_test_boat(grid, conteur_col + 3, conteur_line + 4) != 0)
+			return 1;
+	}
+	if (ft_test_born(conteur_col + 4, conteur_line + 1) == 0){
+		if (ft_test_boat(grid, conteur_col + 4, conteur_line + 1) != 0)
+			return 1;
+	}
+
+	if (indice_col < 13 && indice_line < 15){
+		grid[indice_line][indice_col] = 'O';
+		grid[indice_line + 1][indice_col] = 'O';
+		grid[indice_line][indice_col + 1] = 'O';
+		grid[indice_line][indice_col + 2] = 'O';
+		grid[indice_line][indice_col + 3] = 'O';
+		grid[indice_line + 1][indice_col + 1] = 'O';
+		grid[indice_line + 1][indice_col + 2] = 'O';
+		grid[indice_line + 1][indice_col + 3] = 'O';
+		return 0;
+	}
+	return 1;
+}
+
+int ft_boat_1_4_1_3(char **grid, int indice_col, int indice_line){
+	int conteur_col = indice_col;
+	int conteur_line = indice_line;
+
+	if (ft_test_boat(grid, conteur_col, conteur_line) != 0)
+		return 1;
+	if (ft_test_born(conteur_col + 1, conteur_line) == 0){
+		if (ft_test_boat(grid, conteur_col + 1, conteur_line) != 0)
+			return 1;
+	}
+	if (ft_test_born(conteur_col - 1, conteur_line) == 0){
+		if (ft_test_boat(grid, conteur_col - 1, conteur_line) != 0)
+			return 1;
+	}
+	if (ft_test_born(conteur_col, conteur_line - 1) == 0){
+		if (ft_test_boat(grid, conteur_col, conteur_line - 1) != 0)
+			return 1;
+	}
+	if (ft_test_born(conteur_col, conteur_line + 1) == 0){
+		if (ft_test_boat(grid, conteur_col, conteur_line + 1) != 0)
+			return 1;
+	}
+	if (ft_test_born(conteur_col + 1, conteur_line + 1) == 0){
+		if (ft_test_boat(grid, conteur_col + 1, conteur_line + 1) != 0)
+			return 1;
+	}
+	if (ft_test_born(conteur_col - 1, conteur_line + 1) == 0){
+		if (ft_test_boat(grid, conteur_col - 1, conteur_line + 1) != 0)
+			return 1;
+	}
+	if (ft_test_born(conteur_col, conteur_line + 2) == 0){
+		if (ft_test_boat(grid, conteur_col, conteur_line + 2) != 0)
+			return 1;
+	}
+	if (ft_test_born(conteur_col + 1, conteur_line + 2) == 0){
+		if (ft_test_boat(grid, conteur_col + 1, conteur_line + 2) != 0)
+			return 1;
+	}
+	if (ft_test_born(conteur_col - 1, conteur_line + 2) == 0){
+		if (ft_test_boat(grid, conteur_col - 1, conteur_line + 2) != 0)
+			return 1;
+	}
+	if (ft_test_born(conteur_col, conteur_line + 3) == 0){
+		if (ft_test_boat(grid, conteur_col, conteur_line + 3) != 0)
+			return 1;
+	}
+	if (ft_test_born(conteur_col - 1, conteur_line + 3) == 0){
+		if (ft_test_boat(grid, conteur_col - 1, conteur_line + 3) != 0)
+			return 1;
+	}
+	if (ft_test_born(conteur_col, conteur_line + 4) == 0){
+		if (ft_test_boat(grid, conteur_col, conteur_line + 4) != 0)
+			return 1;
+	}
+	if (ft_test_born(conteur_col + 1, conteur_line + 3) == 0){
+		if (ft_test_boat(grid, conteur_col + 1, conteur_line + 3) != 0)
+			return 1;
+	}
+	if (ft_test_born(conteur_col + 1, conteur_line + 4) == 0){
+		if (ft_test_boat(grid, conteur_col + 1, conteur_line + 4) != 0)
+			return 1;
+	}
+	if (ft_test_born(conteur_col + 2, conteur_line + 3) == 0){
+		if (ft_test_boat(grid, conteur_col + 2, conteur_line + 3) != 0)
+			return 1;
+	}
+	if (ft_test_born(conteur_col + 2, conteur_line + 2) == 0){
+		if (ft_test_boat(grid, conteur_col + 2, conteur_line + 2) != 0)
+			return 1;
+	}
+	if (ft_test_born(conteur_col + 3, conteur_line + 3) == 0){
+		if (ft_test_boat(grid, conteur_col + 3, conteur_line + 3) != 0)
+			return 1;
+	}
+	if (ft_test_born(conteur_col + 2, conteur_line + 4) == 0){
+		if (ft_test_boat(grid, conteur_col + 2, conteur_line + 4) != 0)
+			return 1;
+	}
+	if (indice_line < 13 && indice_col < 14){
+		grid[indice_line][indice_col] = 'O';
+		grid[indice_line + 1][indice_col] = 'O';
+		grid[indice_line + 2][indice_col] = 'O';
+		grid[indice_line + 3][indice_col] = 'O';
+		grid[indice_line + 3][indice_col + 1] = 'O';
+		grid[indice_line + 3][indice_col + 2] = 'O';
+		return 0;
+	}
+	return 1;
+}
+
+
 char **ft_fill_grid(char **grid){
 	int indice_col = (rand() % 17);
 	int indice_line = (rand() % 17);
@@ -382,10 +647,28 @@ char **ft_fill_grid(char **grid){
 		indice_col = (rand() % 17);
 		indice_line = (rand() % 17);
 	}
+	indice_col = (rand() % 17);
+	indice_line = (rand() % 17);
+	while (ft_boat_2_2(grid, indice_col, indice_line) != 0){
+		indice_col = (rand() % 17);
+		indice_line = (rand() % 17);
+	}
+	indice_col = (rand() % 17);
+	indice_line = (rand() % 17);
+	while (ft_boat_2_4(grid, indice_col, indice_line) != 0){
+		indice_col = (rand() % 17);
+		indice_line = (rand() % 17);
+	}
+	indice_col = (rand() % 17);
+	indice_line = (rand() % 17);
+	while (ft_boat_1_4_1_3(grid, indice_col, indice_line) != 0){
+		indice_col = (rand() % 17);
+		indice_line = (rand() % 17);
+	}
 	return grid;
 }
 
-int main(void){
+char **cree_tableau(void){
 	char **grid;
 	int conteur_col;
 	int conteur_line;
@@ -401,10 +684,5 @@ int main(void){
 	}
 	grid[conteur_line][0] = '\0';
 	grid = ft_fill_grid(grid);
-	for (conteur_line = 0; conteur_line < 17; conteur_line++){
-		for (conteur_col = 0; conteur_col < 17; conteur_col++)
-			printf("%c", grid[conteur_line][conteur_col]);
-		printf("\n");
-	}
-	return 0;
+	return grid;
 }
