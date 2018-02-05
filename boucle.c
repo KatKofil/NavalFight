6b988cae539306d173988017485a3406f041e3c6
Yrrieth <thierry.wen@laposte.net>
2018-02-06 00:53:03 +0100

    Amélioration de la saisie dans la fonction attaquer

--- boucle.c
+++ boucle.c
@@ -14,7 +14,7 @@ char * nomjoueur(){
   return chaine;
 }
 
-void viderbuffer(void){
+void viderbuffer(void){  // Vide le buffer (Annule l'entrée '\n' au clavier)
   int c = 0;
   while(c != '\n' && c != EOF){
     c = getchar();
@@ -36,62 +36,68 @@ void insert(char **grid, int ligne  , int colonne){
 // Verifie si case vide 
 
 int verifie (char **grid, int ligne  , int colonne){
-     
-     if(grid[ligne][colonne]== '.'){
-      insert(grid, ligne, colonne);
-      return 0;
-     }
-
-      if(grid[ligne][colonne]== 'O'){ // bea
-        insert(grid, ligne, colonne);
-        return 0;        
-      }
-      if(grid[ligne][colonne]== '*' || grid[ligne][colonne]== '+'|| grid[ligne][colonne]== 'X'){
-        printf("Impossible de jouer\n");
-        // demande au joueur de choisir une autre case 
-        return 1;
-      }
-      return 1;
+  if(grid[ligne][colonne]== '.'){
+    insert(grid, ligne, colonne);
+    return 0;
+  }
+  if(grid[ligne][colonne]== 'O'){ // bea
+    insert(grid, ligne, colonne);
+    return 0;        
+  }
+  if(grid[ligne][colonne]== '*' || grid[ligne][colonne]== '+'|| grid[ligne][colonne]== 'X'){
+    printf("Impossible de jouer, case déjà attaquée\n");
+    // demande au joueur de choisir une autre case 
+    return 1;
+  }
+  return 1;
 }
 
 int attaquer(char **grid) {
-  int ligne, colonne;
+  char ligne;
+  int colonne;
+  int conditionligne = 1;
+  int conditioncolonne = 1;
   int i, j;    // Compteur plateau
-  printf("Choississez la ligne :\n");
-  scanf("%d", &ligne);
-  printf("Choississez la colonne :\n");
-  scanf("%d", &colonne);
-
-  if ((ligne < 0 || ligne > 17) || colonne < 0 || ligne > 17) {  // Si les coordonnes choisis sont en dehors du tableau
-    return 1;
+  int casevalide;
+  while (conditionligne == 1) {
+    printf("Choississez la ligne (lettre):\n");
+    scanf("%c", &ligne);
+    if (ligne >= 97 && ligne <= 113) {
+      ligne -= 97;
+      conditionligne = 0;
+    } else {
+      printf("Veuillez saisir une lettre minuscule compris entre a et q\n");
+    }
+    viderbuffer();
   }
+  conditionligne = 1;
+
+  while (conditioncolonne == 1) {
+    printf("Choississez la colonne (nombre):\n");
+    scanf("%d", &colonne);
+    if (colonne >= 0 && colonne <= 16) {
+      conditioncolonne = 0;
+    } else {
+      printf("Veuillez saisir un nombre compris entre 0 et 16\n");
+    }
+    viderbuffer();
+  }
+  conditioncolonne = 1;
 
   for (i = 0; i < 17; i++) {
     for (j = 0; j < 17; j++) {
       if (i == ligne && j == colonne) {
-	verifie(grid, ligne, colonne);
-	return 0;
-	/* } else {
-	 * p->plateau[i][j] = 'X';
-	 */
-	
+        casevalide = verifie(grid, ligne, colonne);
+	if (casevalide == 0) {
+	  return 0;
+	} else {
+	  return 1;
+	}
       }
     }
   }
   return 1;
 }
-/*
-void affiche(char** grid) {
-  int i, j;
-  for (i = 0; i < 17; i++) {
-    for (j = 0; j < 17; j++) {
-      printf("%3c", grid[i][j]);
-      if (j == 16) {
-	printf("\n");
-      }
-    }
-  }
-}*/
 
 void boucle(char **grid1, char **grid2) {
   int compteurprincipale, compteurjoueurun, compteurjoueurdeux;
@@ -102,50 +108,34 @@ void boucle(char **grid1, char **grid2) {
   char *joueur1 = nomjoueur();
   char *joueur2 = nomjoueur();
   
-
-  
   while(compteurprincipale == 0) {  // Boucle principale du jeu
 
-    printf("\n--------------------------------------------------------------------------------------------\n\n\n");
+    printf("\n------------------------------------------------------------------------------------------\n\n\n");
     
     while(compteurjoueurun == 0) {  // Boucle joueur 1
       printf("Grille de %s\nSélectionnez la ligne et la colonne à attaquer :\n", joueur1);
       afficher(grid1);
       if (attaquer(grid2) == 0) {
-	
 	compteurjoueurun = 1;
+      } else {
+	printf("Veuillez ressaisir les coordonnées. \n");
       }
       
     }
     compteurjoueurun = 0;
-    
-    /* if (fonction qui dit que tous est coule est vrai) 
-     * printf("Gagne !\n");
-     * return;
-     */
 
-    printf("\n--------------------------------------------------------------------------------------------\n\n\n");
+    printf("\n------------------------------------------------------------------------------------------\n\n\n");
 
     while(compteurjoueurdeux == 0) {  // Boucle joueur 2
       printf("Grille de %s\nSélectionnez la ligne et la colonne à attaquer :\n", joueur2);
       afficher(grid2);
       if (attaquer(grid1) == 0) {
-	
-	
 	compteurjoueurdeux = 1;
+      } else {
+	printf("Veuillez ressaisir les coordonnées. \n");
       }
     }
     compteurjoueurdeux = 0;
-
-    /* if (fonction qui dit que tous est coule est vrai) 
-     * printf("Gagne !\n");
-     * return;
-     */
-
-    
-
-    viderbuffer();
-
   }
   return;
 }
