6c12db5c7d899ecb81828eb4774ac65d3248cc82
Yrrieth <thierry.wen@laposte.net>
2018-02-06 20:22:09 +0100

    Modifié la condition des boucles

--- defaite.c
+++ defaite.c
@@ -1,13 +1,13 @@
 #include "navalfight.h"
 
 int ft_detect_defaite(char **grid){
-	int conteur_col;
-	int conteur_line;
+  int conteur_col;
+  int conteur_line;
 
-	for (conteur_line = 0; grid[conteur_line][conteur_col] != NULL; conteur_line++){
-		for (conteur_col = 0; grid[conteur_line][conteur_col] != NULL; conteur_col++)
-			if (grid[conteur_line][conteur_col] == 'O')
-				return (1);
-	}
-	return 0;
+  for (conteur_line = 0; conteur_line < 18; conteur_line++){
+    for (conteur_col = 0; conteur_col < 18; conteur_col++)
+      if (grid[conteur_line][conteur_col] == 'O')
+	return (1);
+  }
+  return 0;
 }
