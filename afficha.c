d7307b0ae33af5b45177b38fc820d40ad49a107d
aimu97 <aimuna.ayadi97@gmail.com>
2018-03-16 10:57:46 +0100

    modification de la grille d'affichage

--- afficha.c
+++ afficha.c
@@ -2,14 +2,14 @@
 
 
 void regle(){
-printf("------------------------------------Bataille Naval--------------------------------------\n");
-printf("Règles :\n");
-printf("\n");
-printf(". = Vide\n");
-printf("* = Raté \n");
-printf("X = coulé \n");
-printf("+ = touché \n");
-printf("\n");
+  printf("------------------------------------Bataille Naval--------------------------------------\n");
+  printf("Règles :\n");
+  printf("\n");
+  printf(". = Vide\n");
+  printf("* = Raté \n");
+  printf("X = coulé \n");
+  printf("+ = touché \n");
+  printf("\n");
 
 }
 
@@ -27,7 +27,10 @@ void afficher(char** grille){
   printf("\n");
 
   for(ligne = 0 ; ligne < TAILLE_GRID ; ++ligne){
-    printf("-------------------------------------------------------------------------------------\n");
+    for(int i = 0 ; i < TAILLE_GRID ; i++)
+      printf("-----");
+    printf("-\n");
+    
    
     for(colonne = 0 ; colonne < TAILLE_GRID ; ++colonne)
       printf("| %c  ",grille [ligne][colonne]);
@@ -41,6 +44,10 @@ void afficher(char** grille){
     
     printf("\n");
   }
-  printf("-------------------------------------------------------------------------------------\n");
+  for(int l = 0 ; l < TAILLE_GRID ; l++)
+    printf("-----");
+      printf("-\n");
+      
+  
 }
 
