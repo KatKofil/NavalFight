d069370eb50a62cf50bc8922fbe4b01de5ff36e6
WEN Thierry <twen@piscine-29.bocal.cs.univ-paris8.fr>
2018-03-23 09:51:49 +0100

    Ajout commentaire

--- boucle.c
+++ boucle.c
@@ -140,7 +140,7 @@ void boucle(char **grid1, char **grid2) {
     while(compteurjoueurun == 0) {  // Boucle joueur 1
       printf("Grille de %s\nSélectionnez la ligne et la colonne à attaquer :\n", joueur1);
       afficher(grid1);
-      if (attaquer(grid2) == 0) {
+      if (attaquer(grid2) == 0) {  // Si on a réussi à placer une lettre, on sort de la boucle
 	compteurjoueurun = 1;
       } else {
 	printf("Veuillez ressaisir les coordonnées. \n");
@@ -154,7 +154,7 @@ void boucle(char **grid1, char **grid2) {
     while(compteurjoueurdeux == 0) {  // Boucle joueur 2
       printf("Grille de %s\nSélectionnez la ligne et la colonne à attaquer :\n", joueur2);
       afficher(grid2);
-      if (attaquer(grid1) == 0) {
+      if (attaquer(grid1) == 0) {  // Si on a réussi à placer une lettre, on sort de la boucle
 	compteurjoueurdeux = 1;
       } else {
 	printf("Veuillez ressaisir les coordonnées. \n");
