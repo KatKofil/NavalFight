753b62b05ff86d2b3c3eaf09b95d788531d085c7
Yrrieth <thierry.wen@laposte.net>
2018-04-12 22:49:32 +0200

    Maj condition

--- put_couler.c
+++ put_couler.c
@@ -29,7 +29,7 @@ int put_couler_gauche(int x,int y, char ** grid, int conteur){
 		return 0;
 	if (x - 1 > -1)
 		resultat += put_couler_gauche(x - 1, y, grid, conteur);
-	if (y + 1 < 17)
+	if (y + 1 < TAILLE_GRID)
 		resultat += put_couler_bas(x, y + 1, grid, conteur);
 	if (y - 1 > -1)
 		resultat += put_couler_haut(x, y - 1, grid, conteur);
@@ -47,7 +47,7 @@ int put_couler_haut(int x,int y, char ** grid, int conteur){
 		return 0;
 	if (y - 1 > -1)
 		resultat += put_couler_haut(x, y - 1, grid, conteur);
-	if (x + 1 < 17)
+	if (x + 1 < TAILLE_GRID)
 		resultat += put_couler_droite(x + 1, y, grid, conteur);
 	if (x - 1 > -1)
 		resultat += put_couler_gauche(x - 1, y, grid, conteur);
@@ -64,9 +64,9 @@ int put_couler_bas(int x,int y, char ** grid, int conteur){
 		return 0;
 	if (grid[x][y] == '.' || grid[x][y] == '*')
 		return 0;
-	if (y + 1 < 17)
+	if (y + 1 < TAILLE_GRID)
 		resultat += put_couler_bas(x, y + 1, grid, conteur);
-	if (x + 1 < 17)
+	if (x + 1 < TAILLE_GRID)
 		resultat += put_couler_droite(x + 1, y, grid, conteur);
 	if (x - 1 > -1)
 		resultat += put_couler_gauche(x - 1, y, grid, conteur);
@@ -77,11 +77,11 @@ int put_couler(int x,int y,char ** grid){
 	int resultat = 0;
 	if (grid[x][y] == '+')
 		grid[x][y] = 'X';
-	if (x + 1 < 17)
+	if (x + 1 < TAILLE_GRID)
 		resultat += put_couler_droite(x + 1, y, grid, 0);
 	if (x - 1 > -1)
 		resultat += put_couler_gauche(x - 1, y, grid, 0);
-	if (y + 1 < 17)
+	if (y + 1 < TAILLE_GRID)
 		resultat += put_couler_bas(x, y + 1, grid, 0);
 	if (y - 1 > -1)
 		resultat += put_couler_haut(x, y - 1, grid, 0);
