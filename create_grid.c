7236ba1fa86d8c2d1c30d2aa3c321a0955cd898e
Yrrieth <thierry.wen@laposte.net>
2018-04-13 01:32:38 +0200

    Maj fonction de vérification de placement disponible, version finale

--- create_grid.c
+++ create_grid.c
@@ -31,38 +31,50 @@ int ft_boat_verifie(char **grid, int indice_col, int indice_line, int size_col, 
   for (indice_line; indice_line < conteur_line + size_line; indice_line++)  
     for (indice_col = conteur_col; indice_col < conteur_col + size_col; indice_col++) {
 
-      // Si on est sur la ligne 0 ou la colonne 0
-      if (indice_col == 0 || indice_line == 0) { 
-	//if (ft_test_born(indice_col, indice_line) == 0)
-	  if (ft_test_boat(grid, indice_col, indice_line) != 0 || ft_test_boat(grid, indice_col + 1, indice_line) != 0 || ft_test_boat(grid, indice_col, indice_line + 1) != 0) {
+      // Si on dépasse de la grille
+      if (ft_test_born(indice_col, indice_line) == 1)
+	return 1;
+      
+      // Pour les cases particulières (indice_col, indice_line valent 0 ou TAILLE_GRID - 1
+      ///////////////////////////////
+
+      // Si une partie du bateau se trouve sur la ligne 0 ou la colonne 0
+      if (indice_col == 0 || indice_line == 0) {
+	// Si il existe un bateau, on retourne 1
+	  if (ft_test_boat(grid, indice_col, indice_line) != 0 || ft_test_boat(grid, indice_col - 1, indice_line) != 0 || ft_test_boat(grid, indice_col + 1, indice_line) != 0 || ft_test_boat(grid, indice_col, indice_line + 1) != 0) {
 	    return 1;
 	  } else {
 	    continue;
 	  }
       }
 
-      /* if (indice_col >= TAILLE_GRID - size_col - 1 || indice_line >= TAILLE_GRID - size_line - 1) {
+      // Si une partie du bateau PEUT se trouver sur la ligne ou la colonne TAILLE_GRID - 1
+      if (indice_col >= TAILLE_GRID - size_col - 1 || indice_line >= TAILLE_GRID - size_line - 1) {
+	// Si on dépasse de la grille
 	if (ft_test_born(indice_col, indice_line) == 0) {
-	  if (ft_test_boat(grid, indice_col, indice_line) != 0 || ft_test_boat(grid, indice_col - 1, indice_line) != 0 || ft_test_boat(grid, indice_col, indice_line - 1) != 0) {
+
+	  // Alors, si on a pas encore atteint TAILLE_GRID - 1
+	  // ON Vérifie si il y a un bateau sur les cases adjacentes
+	  if (ft_test_boat(grid, indice_col, indice_line) != 0 || ft_test_boat(grid, indice_col - 1, indice_line) != 0 || ft_test_boat(grid, indice_col + 1, indice_line) != 0 || ft_test_boat(grid, indice_col, indice_line + 1) != 0 || ft_test_boat(grid, indice_col, indice_line - 1) != 0) {
 	    return 1;
 	  } else {
 	    continue;
 	  }
-	  
 	} else {
 	  return 1;
 	}
-	}*/
-	
+      }
+      
+      
       // Sinon, pour les autres cases
+      ///////////////////////////////
 	
-      // Vérifie sur la case actuelle et adjacente si on ne dépasse pas de la grille
-      if (ft_test_born(indice_col, indice_line) == 0 && ft_test_born(indice_col - 1, indice_line) == 0 && ft_test_born(indice_col + 1, indice_line) == 0 && ft_test_born(indice_col, indice_line - 1) == 0 && ft_test_born(indice_col, indice_line + 1) == 0) { 
-
-	if (ft_test_boat(grid, indice_col, indice_line) != 0 || ft_test_boat(grid, indice_col - 1, indice_line) != 0 || ft_test_boat(grid, indice_col + 1, indice_line) != 0 || ft_test_boat(grid, indice_col, indice_line + 1) != 0 || ft_test_boat(grid, indice_col, indice_line - 1) != 0) {  
+      // Vérifie sur la case actuelle et adjacentes si on ne dépasse pas de la grille
+      if (ft_test_born(indice_col, indice_line) == 0 && ft_test_born(indice_col - 1, indice_line) == 0 && ft_test_born(indice_col + 1, indice_line) == 0 && ft_test_born(indice_col, indice_line - 1) == 0 && ft_test_born(indice_col, indice_line + 1) == 0) {
+	
+	// Vérifie sur la case actuelle et adjacentes si il y a un bateau
+	if (ft_test_boat(grid, indice_col, indice_line) != 0 || ft_test_boat(grid, indice_col - 1, indice_line) != 0 || ft_test_boat(grid, indice_col + 1, indice_line) != 0 || ft_test_boat(grid, indice_col, indice_line + 1) != 0 || ft_test_boat(grid, indice_col, indice_line - 1) != 0) 
 	  return 1;
-	}
-	  
       } else { // Sinon, on a dépassé de la grille
 	return 1;
       }
@@ -72,7 +84,9 @@ int ft_boat_verifie(char **grid, int indice_col, int indice_line, int size_col, 
   return 0;
 }
 
-int ft_boat_put(char **grid, int indice_col, int indice_line, int size_col, int size_line) {  // Fonction qui appelle les vérifications et met un caractère
+// Fonction qui appelle les vérifications et met un caractère
+
+int ft_boat_put(char **grid, int indice_col, int indice_line, int size_col, int size_line) { 
   int conteur_col = indice_col;
   int conteur_line = indice_line;
 
@@ -80,22 +94,15 @@ int ft_boat_put(char **grid, int indice_col, int indice_line, int size_col, int 
     return 1;
   }
 
-  if (ft_boat_verifie(grid, indice_col, indice_line, size_col, size_line) != 0)
+  if (ft_boat_verifie(grid, indice_col, indice_line, size_col, size_line) != 0) // Si on échoue les tests de placements
     return 1;
-
-
-  
   
   for (conteur_line; conteur_line < indice_line + size_line; conteur_line++){  // Met le caractere 'O'
     for (conteur_col = indice_col; conteur_col < indice_col + size_col; conteur_col++) {
-      printf("cont col %d cont lin %d size col %d size lin %d\n",conteur_col, conteur_line, size_col, size_line);
-      //if ((conteur_col < TAILLE_GRID - size_col - 1) || (conteur_line < TAILLE_GRID - size_line - 1)){
-	grid[conteur_line][conteur_col] = 'O';
-	//}
+      grid[conteur_line][conteur_col] = 'O';
     }
   }
   return 0;
-  
 }
 
 // Choisis des coordonnées aléatoires
@@ -104,6 +111,7 @@ char **ft_fill_grid(char **grid, int size_col, int size_line){
 	int indice_col = (rand() % TAILLE_GRID);
 	int indice_line = (rand() % TAILLE_GRID);
 
+	// Tant qu'on a pas pas réussi à mettre un bateau dans la grille, on choisis de nouvelles coordonnées
 	while (ft_boat_put(grid, indice_col, indice_line, size_col, size_line) != 0){
 		indice_col = (rand() % TAILLE_GRID);
 		indice_line = (rand() % TAILLE_GRID);
@@ -114,7 +122,7 @@ char **ft_fill_grid(char **grid, int size_col, int size_line){
 	return grid;
 }
 
-// Cree une grille et appelle la fonction de placement des bateaux
+// Crée une grille et appelle la fonction de placement des bateaux
 
 char **cree_tableau(void){
 	char **grid;
@@ -131,36 +139,24 @@ char **cree_tableau(void){
 	}
 	grid[conteur_line][0] = '\0';
 
-	
-
-	//grid = ft_fill_grid(grid, 18, 18);
-	
-	grid = ft_fill_grid(grid, 2, 2);
-	grid = ft_fill_grid(grid, 2, 2);
-	grid = ft_fill_grid(grid, 2, 2);
-	grid = ft_fill_grid(grid, 2, 2);
-	grid = ft_fill_grid(grid, 2, 2);
-	
-	grid = ft_fill_grid(grid, 2, 2);
-	grid = ft_fill_grid(grid, 2, 2);
-	grid = ft_fill_grid(grid, 2, 2);
-	grid = ft_fill_grid(grid, 2, 2);
-	grid = ft_fill_grid(grid, 2, 2);
-	
-	grid = ft_fill_grid(grid, 2, 2);
-	grid = ft_fill_grid(grid, 2, 2);
-	grid = ft_fill_grid(grid, 2, 2);
-	grid = ft_fill_grid(grid, 2, 2);
-	grid = ft_fill_grid(grid, 2, 2);
+	// Ici, modifiez le nombre et la taille des bateaux
+	// Selon la taille de la grille
 
 	grid = ft_fill_grid(grid, 2, 2);
 	grid = ft_fill_grid(grid, 2, 2);
 	grid = ft_fill_grid(grid, 2, 2);
 	grid = ft_fill_grid(grid, 2, 2);
 	grid = ft_fill_grid(grid, 2, 2);
-	
-	grid = ft_fill_grid(grid, 2, 2);
 
+        grid = ft_fill_grid(grid, 3, 2);
+	grid = ft_fill_grid(grid, 2, 1);
+	grid = ft_fill_grid(grid, 1, 2);
+	grid = ft_fill_grid(grid, 3, 2);
+	grid = ft_fill_grid(grid, 3, 2);
+
+	grid = ft_fill_grid(grid, 2, 3);
+	grid = ft_fill_grid(grid, 2, 3);
+	grid = ft_fill_grid(grid, 1, 1);
 	
 	return grid;
 }
